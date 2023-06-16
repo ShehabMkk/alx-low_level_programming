5c19b35ff07e511340c10169c8c94518ddf9eaa1
ShehabMkk <shehab.mkk2004@gmail.com>
2023-06-16 08:07:57 -0700

    1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,27 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 99; a++)
-  	{
-    		for (b = 0; b <= 99; b++)
-    		{
-      			if (a < b)
-      			{
-        			putchar('0' + (a / 10));
-        			putchar('0' + (a % 10));
-        			putchar(' ');
-        			putchar('0' + (b / 10));
-        			putchar('0' + (b % 10));
-        			if (a != 98 || b != 99)
-				{
-          				putchar(',');
-          				putchar(' ');
-        			}
-      			}
-    		}
-  	}
-
-  	putchar('\n');
-
-  	return 0;
+for (a = 0; a <= 99; a++)
+{
+for (b = 0; b <= 99; b++)
+{
+if (a < b)
+{
+putchar('0' + (a / 10));
+putchar('0' + (a % 10));
+putchar(' ');
+putchar('0' + (b / 10));
+putchar('0' + (b % 10));
+if (a != 98 || b != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
 }
 
+putchar('\n');
+
+return 0;
+}
