8494db1167fdd579aeb40fcae1f43a97913c48b2
ShehabMkk <shehab.mkk2004@gmail.com>
2023-06-16 07:59:09 -0700

    1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible different combinations of two two-digit numbers
+ * main - prints all possible different combinations of three digits
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void) 
+{
   int a, b;
 
-  for (a = 0; a <= 99; a++) {
-    for (b = a + 1; b <= 99; b++) {
-      putchar('0' + (a / 10));
-      putchar('0' + (a % 10));
-      putchar(' ');
-      putchar('0' + (b / 10));
-      putchar('0' + (b % 10));
-      putchar(',');
-      putchar(' ');
+  for (a = 0; a <= 99; a++) 
+  {
+    for (b = 0; b <= 99; b++) 
+    {
+      if (a < b) 
+      {
+        putchar('0' + (a / 10));
+        putchar('0' + (a % 10));
+        putchar(' ');
+        putchar('0' + (b / 10));
+        putchar('0' + (b % 10));
+        if (a != 98 || b != 99) 
+	{
+          putchar(',');
+          putchar(' ');
+        }
+      }
     }
   }
 
@@ -23,5 +32,4 @@ int main(void) {
 
   return 0;
 }
-
 
