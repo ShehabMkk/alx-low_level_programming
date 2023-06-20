f25aa1597cfcaa4078397cfa778936c096db756f
ShehabMkk <shehab.mkk2004@gmail.com>
2023-06-20 15:08:00 -0700

    1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include<main.h>
 /**
  * main - main
  * Return: 0.
  */
 int main(void)
 {
-	_putchar('_'), _putchar('p');
+	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
