ab55d7cc8c0c61d684b285f30172f1aaae7004ca
ShehabMkk <shehab.mkk2004@gmail.com>
2023-06-23 06:37:12 -0700

    1

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,34 +1,32 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - print
- * Return: Always 0
+ * print_triangle - print
+ * @size: int
  */
-int main(void)
+void print_triangle(int size)
 {
-	int i;
-
-	for (i = 1; i <= 100; i++)
+	if (size <= 0)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		} else if (i == 1)
-		{
-			printf("%d", i);
-		} else
-		{
-			printf(" %d", i);
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
 		}
 	}
-	printf("\n");
-
-	return (0);
 }
